52b066caf28550ad9cb8a33b141edc4d0f8ea7c1
Ali Alsayed <a_alsayed87@hotmail.com>
2019-11-12 10:03:44 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -39,7 +39,7 @@ class LoginForm extends React.Component {
           </label>
         </div>
         <div>
-          <button type="submit" onSubmit={this.handleSubmit}>Log in</button>
+          <button type="submit">Log in</button>
         </div>
       </form>
     );
